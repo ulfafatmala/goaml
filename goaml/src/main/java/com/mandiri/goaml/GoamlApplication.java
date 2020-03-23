e76c1ed9043fba6404656104c243db6ec2989d83
ulfafatmala <56835923+ulfafatmala@users.noreply.github.com>
2020-03-23 11:24:45 +0700

    master goaml
    tes master

---
+++ goaml/src/main/java/com/mandiri/goaml/GoamlApplication.java
@@ -0,0 +1,13 @@
+package com.mandiri.goaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoamlApplication.class, args);
+	}
+
+}
---
+++ goaml/src/test/java/com/mandiri/goaml/GoamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandiri.goaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoamlApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
